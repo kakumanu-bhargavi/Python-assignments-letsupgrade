14da9c556dd278d622657faff5030df03f2bdf53
K.V.J.S.BHARGAVI <68589601+kakumanu-bhargavi@users.noreply.github.com>
2020-07-23 00:07:57 +0530

    Add files via upload

---
+++ day3 assignment.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+num = int(input("enter a value of n:"))
+hold = num
+sum1 = 0
+if num <= 0:
+    print("Enter a positive number")
+else:
+    while num > 0:
+        sum1 = sum1 + num
+        num = num - 1;
+    print("the sum of",hold, "natural num is:", sum1)
+
+
+# In[18]:
+
+
+num = int(input("Enter a number"))
+if num>1:
+    for i in range(2,num):
+        if(num%i)==0:
+            print(num,"is not a prime number")
+            print(i,"times",num//i,"is",num)
+            break
+    else:
+        print(num,"is a prime number")
+else:
+    print(num,"is not a prime number")
+
+
+# In[ ]:
+
+
+
+
