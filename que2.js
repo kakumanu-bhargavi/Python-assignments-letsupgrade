e6a514ed1545e7a7f0f00e9fb0a1a1be4401cad5
K.V.J.S.BHARGAVI <68589601+kakumanu-bhargavi@users.noreply.github.com>
2020-09-04 17:00:32 +0530

    Delete que2.js

--- que2.js
+++
@@ -1,7 +0,0 @@
-// 2.program to convert minutes to seconds
-function convertMinutestoSeconds(minutes)
-{
-    return Math.floor(minutes * 60);
-}
-var minutesToSeonds = convertMinutestoSeconds(2);
-console.log("result " +minutesToSeonds, "seconds")
