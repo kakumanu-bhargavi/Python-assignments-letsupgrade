3dadffe278ad22d89b413612b5466eb787475aec
K.V.J.S.BHARGAVI <68589601+kakumanu-bhargavi@users.noreply.github.com>
2020-09-10 19:04:42 +0530

    Add files via upload

---
+++ JS assignments/domman.js
@@ -0,0 +1,23 @@
+function doSomething()
+{
+    const ele = document.getElementById("image");
+    const newUrl = "https://image.cnbcfm.com/api/v1/image/104819285-thor.jpg?v=1529476684";
+
+    ele.src = newUrl;
+}
+
+function changeImage()
+{
+  const ele = document.getElementById("image");
+  const newUrl = "https://i.ytimg.com/vi/u-l6PYcX6AA/maxresdefault.jpg";
+
+  ele.src = newUrl;
+}
+
+function printValue()
+{
+    const ele = document.getElementById("image");
+    const newUrl = "https://i.ytimg.com/vi/7YrkvUW84RM/maxresdefault.jpg";
+
+    ele.src = newUrl;
+}
